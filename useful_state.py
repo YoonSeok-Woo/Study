fb03351dd6744fc3810090632e23ec6580548820
YoonSeok Woo <wysf1994@naver.com>
2021-07-20 16:34:29 +0900

    0720_1

---
+++ useful_state.py
@@ -0,0 +1,10 @@
+a = [1,2,3,4,5]
+reverse_a = a[::-1]
+between_a_ra = [*a,'bet',*reverse_a]
+front_a = a[:3]
+back_a = a[3:]
+print(a)
+print(reverse_a)
+print(between_a_ra)
+print(front_a)
+print(back_a)
