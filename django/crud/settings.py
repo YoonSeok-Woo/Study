2510934bd83f8e056735ffc114f45587a6fb9cd9
YoonSeok Woo <wysf1994@naver.com>
2021-09-08 18:29:34 +0900

    0908

--- django/crud/settings.py
+++ django/crud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = 'django-insecure--i_f#7+e5@2w!swuyuv8gcmj@jj1mw0pc&$m2esymvnl6uq3(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
